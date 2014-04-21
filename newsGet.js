0b51edf1be679bf1523ed979a5f21954b9c87101
NI54 <isco_s@hotmail.com>
2014-04-21 11:40:27 -0300

    asdas
    asdasas

--- newsGet.js
+++ newsGet.js
@@ -2,7 +2,7 @@
 //var urlPhp='http://localhost/server/getArticlesR.php?cu=';
 
 
-var urlArt='http://www.ni54.com/Racing/article/art';
+var urlArt='http://nuevosociales.com.ar/app/article/art';
 //var urlPhp='http://www.ni54.com/Racing/getArticlesR.php?cu=';
 var urlPhp='http://nuevosociales.com.ar/app/getArticlesR.php?cu=';
 var urlImagenes= 'http://nuevosociales.com.ar/app/';
@@ -49,7 +49,7 @@ $( "#testList" ).on( "pagebeforecreate",function(){
 			titleRep= titleRep.replace(/replaceComillasSimples/g,"'");
 			
 			$( '#nota' ).html("");
-			$( '#nota' ).append('<div style="width:30%;height:12em;float:left;" ><img src="'+urlImagenes++imagen+'" style="width:100%;height:auto;border:1px solid black;top:0;border-radius:10px;"></div>');
+			$( '#nota' ).append('<div style="width:30%;height:12em;float:left;" ><img src="'+urlImagenes+imagen+'" style="width:100%;height:auto;border:1px solid black;top:0;border-radius:10px;"></div>');
 			$( '#nota' ).append("<div style= 'width:68%;height:12em;display:inline-block;position:relative;float:left;top:-2em;padding-left:2%;'><p style='font-size:2em;'>"+ titleRep +"</p><p>"+ date +"</p></div>");
 			//$( '#nota' ).append("<h8 style='display:block'>"+ date +"</h8></div>");
 			$( '#nota' ).append('<p></p>');
