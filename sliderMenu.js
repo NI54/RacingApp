b67bc11d639178196243600f5754b355be71d20e
NI54 <isco_s@hotmail.com>
2014-04-22 14:35:32 -0300

    sdfsdfsdf
    sdfsdfsdfsdfs

--- sliderMenu.js
+++ sliderMenu.js
@@ -1,4 +1,4 @@
-$('#testList').append(' <div data-role="panel" id="left-panel" data-theme="c" data-display="push" style="border:none;position:fixed;"><li data-role="list-divider">Menu</li><div data-role="controlgroup" corners="false" data-corners="false"><a href="#" class="ui-btn ui-btnSlider" style="text-align: left;"><img src="images/Noticias.png">Noticias</a><a href="#" class="ui-btn ui-btnSlider" style="text-align: left;"><img src="images/Videos.png">Videos</a><a href="#" class="ui-btn ui-btnSlider" style="text-align: left;"><img src="images/Historia.png">Historia</a><a href="#Historia"  class="ui-btn ui-btnSlider" style="text-align: left;"><img src="images/Autoridades.png">Autoridades</a><a href="#" class="ui-btn ui-btnSlider" style="text-align: left;"><img src="images/Publicidad.png">Publicidad</a><a href="#" class="ui-btn ui-btnSlider" style="text-align: left;"><img src="images/Legal.png">Legal</a><a href="#" class="ui-btn ui-btnSlider" style="text-align: left;"><img src="images/Staff.png">Staff</a><a href="#" class="ui-btn ui-btnSlider" style="text-align: left;"><img src="images/Contacto.png">Contacto</a><a href="#" class="ui-btn ui-btnSlider" style="text-align: left;"><img src="images/Estadisticas.png">Estadisticas</a><a href="#" class="ui-btn ui-btnSlider" style="text-align: left;"><img src="images/Plantel.png">Plantel</a> </div></div> ');
+/*$('#testList').append(' <div data-role="panel" id="left-panel" data-theme="c" data-display="push" style="border:none;position:fixed;"><li data-role="list-divider">Menu</li><div data-role="controlgroup" corners="false" data-corners="false"><a href="#" class="ui-btn ui-btnSlider" style="text-align: left;"><img src="images/Noticias.png">Noticias</a><a href="#" class="ui-btn ui-btnSlider" style="text-align: left;"><img src="images/Videos.png">Videos</a><a href="#" class="ui-btn ui-btnSlider" style="text-align: left;"><img src="images/Historia.png">Historia</a><a href="#Historia"  class="ui-btn ui-btnSlider" style="text-align: left;"><img src="images/Autoridades.png">Autoridades</a><a href="#" class="ui-btn ui-btnSlider" style="text-align: left;"><img src="images/Publicidad.png">Publicidad</a><a href="#" class="ui-btn ui-btnSlider" style="text-align: left;"><img src="images/Legal.png">Legal</a><a href="#" class="ui-btn ui-btnSlider" style="text-align: left;"><img src="images/Staff.png">Staff</a><a href="#" class="ui-btn ui-btnSlider" style="text-align: left;"><img src="images/Contacto.png">Contacto</a><a href="#" class="ui-btn ui-btnSlider" style="text-align: left;"><img src="images/Estadisticas.png">Estadisticas</a><a href="#" class="ui-btn ui-btnSlider" style="text-align: left;"><img src="images/Plantel.png">Plantel</a> </div></div> ');*/
 
 
 $( document ).on( "pagecreate", "#testList", function() {
@@ -15,3 +15,23 @@ $( document ).on( "pagecreate", "#testList", function() {
         }
     });
 });
+
+var arraySecciones= new Array();
+var index=0;
+
+function AddSection(name,hide){
+	arraySecciones[index]= name;
+	if(hide==true){
+		$('#'+name).hide();
+	}
+	index++;
+};
+
+function ShowSection(name){
+	 for (a=0;a<arraySecciones.length;a++){
+		if(arraySecciones[a]!=name){
+			$('#'+arraySecciones[a]).hide();
+		}
+	}
+	$('#'+name).show();
+};
