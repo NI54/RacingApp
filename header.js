33ab5fef4e5e169756820a7def88b55895e35574
NI54 <isco_s@hotmail.com>
2014-04-23 11:58:18 -0300

    asdas
    asdasdas

--- header.js
+++ header.js
@@ -4,8 +4,19 @@ $('#testList').append('<div id="header" data-role="header"  style="position:fixe
 $.mobile.orientationChangeEnabled = false;
 
 function InitializeHead(body){ 
-$(body).append('<div id="header" data-role="header"  style="position:fixed;width:100%;z-index:10;">'+'<div data-role="controlgroup" data-type="horizontal" class="ui-btn-left" style="width:20%;left:33%;top:65%;">'+'<a href="#" class="ui-btn ui-btn-twitter" style="padding:0; background-color:transparent;"><img src="images/Twitter.png" style="width:10em;"></a>'+'<a href="#" class="ui-btn ui-btn-twitter" style="padding:0;padding-left: 0.5em; background-color:transparent;"><img src="images/Facebook.png" style="width:10em;"></a>'+'<a href="#" class="ui-btn ui-btn-twitter" style="padding:0; padding-left: 0.5em; background-color:transparent;"><img src="images/Blog.png" style="width:10em;"></a>'+'<a href="#" class="ui-btn ui-btn-twitter" style="padding:0;padding-left: 0.5em; background-color:transparent;"><img src="images/Mail.png" style="width:10em;"></a>'+'</div>'+'<img src="images/Header.png" style="width:25em;height:auto;">'+'</div>');
+$(body).append('<div id="header" data-role="header"  style="position:fixed;width:100%;z-index:10;">'+'<div data-role="controlgroup" data-type="horizontal" class="ui-btn-left" style="width:20%;left:33%;top:65%;">'+'<a href="#" class="ui-btn ui-btn-twitter" style="padding:0; background-color:transparent;"><img src="images/Twitter.png" style="width:10em;"></a>'+'<a href="#" class="ui-btn ui-btn-twitter" style="padding:0;padding-left: 0.5em; background-color:transparent;"><img src="images/Facebook.png" style="width:10em;"></a>'+'<a href="#" class="ui-btn ui-btn-twitter" style="padding:0; padding-left: 0.5em; background-color:transparent;"><img src="images/Blog.png" style="width:10em;"></a>'+'<a href="#" class="ui-btn ui-btn-twitter" style="padding:0;padding-left: 0.5em; background-color:transparent;"><img src="images/Mail.png" style="width:10em;"></a>'+'</div>'+'<img id="imgHeader" src="images/Header.png" style="width:25em;height:auto;">'+'</div>');
 }
+
+$(window).on("orientationchange",function(){
+  if(window.orientation == 0) // Portrait
+  {
+    $("#imgHeader").css({"width:25em;"});
+  }
+  else // Landscape
+  {
+    $("#imgHeader").css({"width:15em"});
+  }
+});
 
 
 //$(document).on("pageinit", "#testList", function () {
